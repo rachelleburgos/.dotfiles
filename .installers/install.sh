bcab6b984d284d5346586eb09ccdf533affe70e2
Rachelle Burgos <rachelle.burgoss@gmail.com>
2023-05-28 00:11:55 -0400

    :)

--- .installers/install.sh
+++ .installers/install.sh
@@ -3,8 +3,18 @@
 # Homebrew Script for OSx
 # To execute: save and `chmod +x ./brew-install-script.sh` then `./brew-install-script.sh`
 
-echo "Installing brew..."
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# Abort on error
+set -e
+
+echo "Checking if Homebrew is already installed...";
+
+# Checks if Homebrew is installed
+if test ! $(which brew); then
+  echo "Installing Homebrew...";
+  yes | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+else
+  echo "Homebrew is already installed...";
+fi
 
 echo "Installing brew cask..."
 brew tap homebrew/cask
@@ -42,4 +52,13 @@ brew install --cask firefox
 echo "Installing some other tools..."
 brew install --cask skim
 brew install --cask alttab
+echo "Installing Alfred dmg file to $HOME/Downloads/"
 curl -L "https://cachefly.alfredapp.com/Alfred_5.1_2134.dmg" -o "$HOME/Downloads/Alfred_5.1_2134.dmg" # Download Alfred 5 to $HOME/Downloads
+
+# Update and Upgrade
+echo "Updating and upgrading Homebrew..."; echo;
+yes | brew update
+yes | brew upgrade
+
+# Remove outdated versions from the cellar
+brew cleanup
